e24fa53802386278b3fafdfa0560581519f2c9d3
Bohdan <bohdan.bedrij@gmail.com>
2025-10-28 11:15:24 +0200

    Added eslint, prettier and husky

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    ignores: [".next/**", "node_modules/**", "dist/**"],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier,
+      react,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
+];
